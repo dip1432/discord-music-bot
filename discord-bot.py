8590cb0c39ce629703582fdae07428574b632a70
Yaroslav <83348992+yaroslavilyin@users.noreply.github.com>
2021-07-31 19:29:18 +0300

    Update discord-bot.py

--- discord-bot.py
+++ discord-bot.py
@@ -10,8 +10,74 @@ async def on_ready():
     print('Bot is online {0.user}'.format(client))
 
 @client.command()
-async def hello(ctx):
-    author = ctx.message.author
-    await ctx.send(f'Hello, {author.mention}!')
+@bot.command()
+async def play(ctx, *, command = None):
+    """Воспроизводит музыку"""
+    global server, server_id, name_channel
+    author = ctx.author
+    if command == None:
+        server = ctx.guild
+        name_channel = author.voice.channel.name
+        voice_channel = discord.utils.get(server.voice_channels, name = name_channel)
+    params = command.split(' ')
+    if len(params) == 1:
+        sourse = params[0]
+        server = ctx.guild
+        name_channel = author.voice.channel.name
+        voice_channel = discord.utils.get(server.voice_channels, name=name_channel)
+        print('param 1')
+    elif len(params) == 3:
+        server_id = params[0]
+        voice_id = params[1]
+        sourse = params[2]
+        try:
+            server_id = int(server_id)
+            voice_id = int(voice_id)
+        except:
+            await ctx.chanell.send(f'{author.mention}, id сервера или войса должно быть целочисленным!')
+            return
+        print('param 3')
+        server = bot.get_guild(server_id)
+        voice_channel = discord.utils.get(server.voice_channels, id=voice_id)
+    else:
+        await ctx.channel.send(f'{author.mention} команда не корректна!')
+        return
 
+    voice = discord.utils.get(bot.voice_clients, guild = server)
+    if voice is None:
+        await voice_channel.connect()
+        voice = discord.utils.get(bot.voice_clients, guild = server)
+
+    if sourse == None:
+        pass
+    elif sourse.startswith('http'):
+        if not check_domains(sourse):
+            await ctx.channel.send(f'{author.mention}, ссылка не является разрешенной!')
+            return
+        song_there = os.path.isfile('music/song.mp3')
+        try:
+            if song_there:
+                    os.remove('music/song.mp3')
+        except PermissionError:
+            await ctx.chennel.send('Недостаточно прав для удаления файла!')
+            return
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [
+                {
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }
+            ],
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([sourse])
+        for file in os.listdir('music/'):
+            if file.endswith('.mp3'):
+                os.rename(file, 'song.mp3')
+        voice.play(discord.FFmpegPCMAudio('music/song.mp3'))
+    else:
+        voice.play(discord.FFmpegPCMAudio(f'music/{sourse}'))
 client.run(settings['token'])
